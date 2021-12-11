aa129bf70c656bb43adb6c2dab4c508a652393cb
hongyi <the_hongyi@163.com>
2021-12-11 23:54:57 +0800

    master-test

---
+++ GitBranchMaster/src/main/java/com/hongyi/demo1.java
@@ -0,0 +1,13 @@
+package com.hongyi;
+
+/**
+ * @version : V1.0
+ * @date : 2021/12/11 23:54
+ * @Author : LLH
+ * @Desc :
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("Master--Test");
+    }
+}
