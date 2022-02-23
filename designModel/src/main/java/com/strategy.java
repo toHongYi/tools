83434f3b1f9fa7c2a493fa7e1e85baae03293fd4
theHongYi <1784640036@qq.com>
2022-02-23 22:01:17 +0800

    frond-end

---
+++ designModel/src/main/java/com/strategy.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @ClassName strategy
+ * @Created by theLin
+ * @Date 2021/12/28 23:48
+ * @Description #{description}
+ */
+public class strategy {
+
+}
--- transform/src/main/java/com/hashMap/Demo01.java
+++ transform/src/main/java/com/hashMap/Demo01.java
@@ -1,7 +1,19 @@
-package com.hashMap;/**
+package com.hashMap;
+
+import java.util.HashMap;
+
+/**
 * @date : 2021/12/22 21:42
 * @Author : LLH
 * @version : V1.0
 * @Desc : 
 */public class Demo01 {
+    public static void main(String[] args) {
+
+        //阅读源码;
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+
+        //阅读源码;
+        Object o = new Object();
+    }
 }
